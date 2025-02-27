cd460e1067dae66dfecb753ee4899d7c6dcb7a9b
chanho0908 <uni070@naver.com>
2025-02-27 15:42:37 +0900

    댓글 디자인 수정

--- js/index.js
+++ js/index.js
@@ -1,45 +1,43 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
-
-console.log("알맞은 스크립트를 작성하세요");
 document.addEventListener("DOMContentLoaded", function () {
-  const commentInput = document.querySelector(".comment-input");
-  const submitButton = document.querySelector(".submit-button");
-  const commentList = document.querySelector(".comment-list");
-  submitButton.addEventListener("click", function () {
-      const commentText = commentInput.value.trim();
-      if (commentText === "") {
-          alert("댓글을 입력해주세요!");
-          return;
-      }
-      const newComment = document.createElement("li");
-      newComment.innerHTML = `
-          <div class="comment-item">
+    const commentInput = document.querySelector(".comment-input");
+    const submitButton = document.querySelector(".submit-button");
+    const commentList = document.querySelector(".comment-list");
+  
+    function addComment() {
+        const commentText = commentInput.value.trim();
+        if (commentText === "") {
+            alert("댓글을 입력해주세요!");
+            return;
+        }
+  
+        const newComment = document.createElement("li");
+        newComment.classList.add("comment-item");
+        newComment.innerHTML = `
             <div class="comment-author">
-              <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
-              <span>방문자</span>
+                <span>방문자</span>
             </div>
-            <div class="comment-content">
-              ${commentText}
-            </div>
-          </div>
-      `;
-      commentList.appendChild(newComment);
-      commentInput.value = "";
+            <div class="comment-content">${commentText}</div>
+            <button class="delete-comment">X</button>
+        `;
+  
+        commentList.appendChild(newComment);
+        commentInput.value = "";
+        commentInput.focus();
+        commentList.scrollTop = commentList.scrollHeight;
+  
+        // 삭제 버튼 기능 추가
+        newComment.querySelector(".delete-comment").addEventListener("click", function () {
+            newComment.remove();
+        });
+    }
+  
+    submitButton.addEventListener("click", addComment);
+  
+    commentInput.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addComment();
+        }
+    });
   });
-});
+  
